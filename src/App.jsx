df2913a8e4dacc6dcedbd227f9142151be9f3a71
Bipin <bipinbhatt280@gmail.com>
2024-03-01 14:21:11 +0545

    Initial set up for frontend

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+
+function App() {
+  return (
+    <ButtonGroup aria-label="Basic example" >
+      <Button variant="primary">hello</Button>
+    </ButtonGroup>
+  );
+}
+export default App
