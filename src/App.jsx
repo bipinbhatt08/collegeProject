62f8baa2b31b286e54af4f64d283693dbd2b92a9
Mahendra Bohara <mbohara991@gmail.com>
2024-03-01 14:56:12 +0545

    Navbar is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
-
+import NavBar from './components/Nav';
 function App() {
-  return (
-    <ButtonGroup aria-label="Basic example" >
+  return (  
+  <>
+  
+  <ButtonGroup aria-label="Basic example" >
       <Button variant="primary">hello</Button>
     </ButtonGroup>
+    <NavBar/>
+</>
   );
 }
 export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+      this is nav bar
+    </div>
+  )
+}
+
+export default Nav
